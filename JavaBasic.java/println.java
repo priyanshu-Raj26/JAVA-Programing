6ed2aeaf438055562de656b4771f960f20557473
Priyanshu Raj <er.priyansu@gmail.com>
2024-09-23 00:03:15 +0530

    Add files via upload

---
+++ JavaBasic.java/println.java
@@ -0,0 +1,11 @@
+public class println {
+    public static void main(String args[]) {
+
+        System.out.println("Hello priyanshu!"); //line ke space
+        System.out.print("Hello priyanshu!\n"); //line ke space
+        System.out.print("Hello\n priyanshu!"); // "\n" ke space
+        
+
+
+    }
+}
